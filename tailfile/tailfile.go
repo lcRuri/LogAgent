4bd02853e8a714132065122b88783210484d5748
lcRuri <1793185558@qq.com>
2023-04-19 13:53:23 +0800

    根据从etcd传输过来的配置文件管理tailtask

--- tailfile/tailfile.go
+++ tailfile/tailfile.go
@@ -1,25 +1,30 @@
 package tailfile
 
 import (
+	"context"
 	"github.com/Shopify/sarama"
 	"github.com/hpcloud/tail"
 	"github.com/sirupsen/logrus"
-	"logagent/common"
 	"logagent/kafka"
 	"strings"
 	"time"
 )
 
 type tailTask struct {
-	path  string
-	topic string
-	tObj  *tail.Tail
+	path   string
+	topic  string
+	tObj   *tail.Tail
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func newTailTask(path, topic string) *tailTask {
+	ctx, cancelFunc := context.WithCancel(context.Background())
 	tt := &tailTask{
-		path:  path,
-		topic: topic,
+		path:   path,
+		topic:  topic,
+		ctx:    ctx,
+		cancel: cancelFunc,
 	}
 
 	return tt
@@ -44,28 +49,31 @@ func (t *tailTask) run() {
 	// TailObj->log->Client->kafka
 	logrus.Infof("collect for path:%s is running...", t.path)
 	for {
-		line, ok := <-t.tObj.Lines
-		if !ok {
-			logrus.Warn("tail file close reopen,filename:%s\n", t.path)
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		//避免空行
-		if len(strings.Trim(line.Text, "\r")) == 0 {
-			logrus.Info("出现空行，跳过...")
-			continue
-		}
-		//利用通道将同步的代码改为异步的
-		//把读出来的一行日志包装成kafka里面的msg类型，丢到通道中
-		msg := &sarama.ProducerMessage{}
-		msg.Topic = t.topic
-		msg.Value = sarama.StringEncoder(line.Text)
+		select {
+		case <-t.ctx.Done(): //调用cancel取消
+			logrus.Infof("path:%s cancel", t.path)
+			return
+		case line, ok := <-t.tObj.Lines:
+			if !ok {
+				logrus.Warn("tail file close reopen,filename:%s\n", t.path)
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			//避免空行
+			if len(strings.Trim(line.Text, "\r")) == 0 {
+				logrus.Info("出现空行，跳过...")
+				continue
+			}
+			//利用通道将同步的代码改为异步的
+			//把读出来的一行日志包装成kafka里面的msg类型，丢到通道中
+			msg := &sarama.ProducerMessage{}
+			msg.Topic = t.topic
+			msg.Value = sarama.StringEncoder(line.Text)
 
-		//丢到管道中
-		kafka.ToMsgChan(msg)
+			//丢到管道中
+			kafka.ToMsgChan(msg)
+
+		}
+
 	}
-}
-
-func SendNewConf(newConf []common.CollectEntry) {
-	ttMgr.confChan <- newConf
 }
--- tailfile/tailfile_mgr.go
+++ tailfile/tailfile_mgr.go
@@ -49,6 +49,56 @@ func Init(allConf []common.CollectEntry) (err error) {
 }
 
 func (t *tailTaskMgr) watch() {
-	//接受新配置
-	newConf := <-t.confChan
+	for {
+		//接受新配置
+		newConf := <-t.confChan
+		logrus.Infof("get new conf from etcd,conf:%v,start managing tailtask", newConf)
+		for _, conf := range newConf {
+			//判断新的配置里面的任务之前是否存在
+			//如果存在 不要动 不存在 添加 如果之前有但是现在没有 停掉
+			//之前存在
+			if t.isExist(conf) {
+				continue
+			}
+			//之前不存在
+			tt := newTailTask(conf.Path, conf.Topic)
+			err := tt.Init()
+			if err != nil {
+				logrus.Errorf("create tailObj for path:%s failed,err:%v", conf.Path, err)
+				return
+			}
+			logrus.Infof("create a tail task for path:%s success", conf.Path)
+			t.tailTaskMap[conf.Path] = tt
+			//后台去收集日志
+			go tt.run()
+		}
+
+		//找到在tailTaskMap里面存在，但是newconf里面不存在的tailtask
+		for key, task := range t.tailTaskMap {
+			var found bool
+			for _, conf := range newConf {
+				if key == conf.Path {
+					found = true
+					break
+				}
+			}
+			//当前key在现在新的conf里面不存在，需要停掉之前的goroutine
+			if !found {
+				//使用task里面的ctx的cancel取消掉goroutine
+				logrus.Infof("the task collect path:%s is stopping", key)
+				delete(t.tailTaskMap, key)
+				task.cancel()
+			}
+		}
+	}
+
+}
+
+func (t *tailTaskMgr) isExist(conf common.CollectEntry) bool {
+	_, ok := t.tailTaskMap[conf.Path]
+	return ok
+}
+
+func SendNewConf(newConf []common.CollectEntry) {
+	ttMgr.confChan <- newConf
 }
