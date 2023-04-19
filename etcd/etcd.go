3629e524b7acdffd88d0710056590a9d2fd301fd
lcRuri <1793185558@qq.com>
2023-04-19 13:41:25 +0800

    新增监听配置文件是否被修改的代码

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -58,20 +58,23 @@ func GetConf(key string) (collectEntryList []common.CollectEntry, err error) {
 
 // 监控etcd里面的日志收集项目里面的变化
 func WatchConf(key string) {
-	watchCh := client.Watch(context.Background(), key)
-	var newConf []common.CollectEntry
-	for wresp := range watchCh {
-		logrus.Info("get new conf from etcd!")
-		for _, evt := range wresp.Events {
-			fmt.Printf("etcd changed:type:%s key:%s value:%s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
-			err := json.Unmarshal(evt.Kv.Value, &newConf)
-			if err != nil {
-				logrus.Errorf("json unmarshal new conf failed,err:%v", err)
-				continue
-			}
+	for {
+		watchCh := client.Watch(context.Background(), key)
+		var newConf []common.CollectEntry
+		for wresp := range watchCh {
+			logrus.Info("get new conf from etcd!")
+			for _, evt := range wresp.Events {
+				fmt.Printf("etcd changed:type:%s key:%s value:%s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
+				err := json.Unmarshal(evt.Kv.Value, &newConf)
+				if err != nil {
+					logrus.Errorf("json unmarshal new conf failed,err:%v", err)
+					continue
+				}
 
-			//告诉tailfile启用新的配置
-			tailfile.SendNewConf(newConf) //没有人接收就是阻塞的
+				//告诉tailfile启用新的配置
+				tailfile.SendNewConf(newConf) //没有人接收就是阻塞的
+			}
 		}
 	}
+
 }
